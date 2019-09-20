530a4c9180385efaf3b0ce4de011489f6f1e961b
maroan <maroan9@gmail.com>
2019-09-20 11:41:45 +0200

    Commit de prueba
    probando

---
+++ prueba.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+function imprime() {
+	print("hola que ase?");
+}
+
+	$Num = 44;
+	$Nombre = "Mario";
+
+	f (true) {
+		print("hola mundo");
+	}
+
+ ?>
+</body>
+</html>
